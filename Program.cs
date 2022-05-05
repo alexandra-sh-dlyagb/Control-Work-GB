34b0d416a239b4db9d4a1c2ae14f4e11c2e8be6c
Alexandra Shutova <axa.shutova@icloud.com>
2022-05-05 23:30:20 +0300

    Final version of programming part

--- Program.cs
+++ Program.cs
@@ -1,36 +1,19 @@
 ﻿string[] ArrayOfStringsInput = new string[] {"Saturday", ":)", "sun", "abcdefg", "qwerty", "o"};//задали массив в начале алгоритма
-string[] NewArray = new string [6]; //создали массив под подходящие под условие элементы
+string[] NewArray = new string [6]; //создали новый массив под подходящие под условие элементы
 int newindex=0;
 for (int index = 0; index<6; index++)
 {
-        int length = ArrayOfStringsInput[index].Length;
-        if (length<=3)
+        int length = ArrayOfStringsInput[index].Length; //определяем длину строки
+        if (length<=3)//условие из задачи
         {
             NewArray[newindex] = ArrayOfStringsInput[index];
             newindex++;
         }
 }
-int count = NewArray.Length;
+int count = NewArray.Length; //выводим массив
 int position = 0;
 while (position < count)
 {
     Console.WriteLine(NewArray[position]);
     position++;
 }
-
-
-
-
-
-
-
-
-//int length = ArrayOfStringsInput[index].Length;
-  //  while (newindex<3)
-    //{
-      //  if (length<=3)//смотрим, удовлетворяет ли элемент условию
-        //{
-          //  NewArray[newindex] = ArrayOfStringsInput[index];//переносим элемент в новый массив
-            //newindex = newindex+1;
-        //}
-    //}
