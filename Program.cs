d2592c7ae63abe5f12e79a6cab9c66b017066c32
Alexandra Shutova <axa.shutova@icloud.com>
2022-05-05 23:38:36 +0300

    Final version of complited programming task

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿string[] ArrayOfStringsInput = new string[] {"Saturday", ":)", "sun", "abcdefg", "qwerty", "o"};//задали массив в начале алгоритма
 string[] NewArray = new string [6]; //создали новый массив под подходящие под условие элементы
+int len = 6;
 int newindex=0;
-for (int index = 0; index<6; index++)
+for (int index = 0; index<len; index++)
 {
         int length = ArrayOfStringsInput[index].Length; //определяем длину строки
         if (length<=3)//условие из задачи
